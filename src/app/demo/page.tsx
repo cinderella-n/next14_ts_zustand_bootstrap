cc22a1a5091a1ea859ac3bc5ff504cd544202783
Nook <cinderella.nook4545@gmail.com>
2024-08-07 15:58:26 +0700

    form control

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+'use client'
+import React, { useState } from 'react'
+import InputNumber from '@/components/atoms/inputnumber'
+import InputText from '@/components/atoms/inputtext'
+type Props = {
 
-const Page = props => {
+}
+const Page = (props: Props) => {
+    const [Age, setAge] = useState<number>(0);
+    const [isValidatedAge, setIsValidatedAge] = useState<boolean | null>(null);
+
+    const handleInputChangeAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = parseFloat(event.target.value);
+        setAge(newValue);
+
+        // ตัวอย่างการตรวจสอบค่าตัวเลข
+        if (newValue >= 1 && newValue <= 10) {
+            setIsValidatedAge(true);
+        } else {
+            setIsValidatedAge(false);
+        }
+    };
+
+    const [inputValue, setInputValue] = useState('');
+    const [isValidated, setIsValidated] = useState<boolean | null>(null);
+
+    const handleInputChangeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = event.target.value;
+        setInputValue(newValue);
+
+        // ตัวอย่างการตรวจสอบความยาว
+        if (newValue.length >= 5 && newValue.length <= 15) {
+            setIsValidated(true);
+        } else {
+            setIsValidated(false);
+        }
+    };
     return (
-        <div>Page</div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <InputText
+                        label="Username"
+                        value={inputValue}
+                        onChange={handleInputChangeUsername}
+                        required={true}
+                        validated={isValidated}
+                        maxLength={15}
+                        minLength={5}
+                    />
+                </div>
+                <div className='col'>
+                    <InputNumber
+                        label="Age"
+                        value={Age}
+                        onChange={handleInputChangeAge}
+                        required={true}
+                        validated={isValidated}
+                        maxValue={10}
+                        minValue={1}
+                    />
+                </div>
+
+            </div>
+        </div>
     )
 }
-
-Page.propTypes = {}
 
 export default Page
