81ba9cd40f3b42cbb3100fa17ae9cf305b879c10
Nook <cinderella.nook4545@gmail.com>
2024-08-13 10:37:02 +0700

    zudstand

---
+++ src/app/demo/zustand/page.tsx
@@ -0,0 +1,18 @@
+// pages/index.tsx
+'use client'
+import react from 'react'
+import { useStore } from '@/stores/useStore';
+
+const Home = () => {
+  const { count, increment, decrement } = useStore();
+
+  return (
+    <div className='container text-center border-2'>
+      <h1 className=' bg-success rounded-2 text-light p-2'>Count:  {count}</h1>
+      <button className='m-2 rounded bg-danger text-light border-1 border-light p-2' onClick={decrement}>- Decrement</button>
+      <button className='m-2 rounded bg-primary text-light border-1 border-light p-2' onClick={increment}>+ Increment</button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/stores/useStore.ts
@@ -0,0 +1,14 @@
+// stores/useStore.ts
+import create from 'zustand';
+
+interface StoreState {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+export const useStore = create<StoreState>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+}));
